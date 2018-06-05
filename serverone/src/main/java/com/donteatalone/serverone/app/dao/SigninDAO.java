0ab3d9976a181061761c5388f26b228c3ccfc82c
Vaibhav Jain <vaibhav8121992@gmail.com>
2018-06-04 19:10:50 -0700

    Integrated SHA-512 for passwords

--- serverone/src/main/java/com/donteatalone/serverone/app/dao/SigninDAO.java
+++ serverone/src/main/java/com/donteatalone/serverone/app/dao/SigninDAO.java
@@ -1,5 +1,8 @@
 package com.donteatalone.serverone.app.dao;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -24,8 +27,17 @@ public class SigninDAO implements ISigninDAO{
 		if(existsUser(user))
 			throw new IllegalArgumentException("User already exists");
 		String sql = "INSERT into Login(email, password) values(?, ?)";
-		jdbcTemplate.update(sql, user.getEmailId(), user.getPasskey());
-		
+		try{
+			MessageDigest m = MessageDigest.getInstance("SHA-512");
+			byte[] bytes = m.digest(user.getPasskey().getBytes());
+			StringBuilder sb = new StringBuilder();
+	        for(int i=0; i< bytes.length ;i++){
+	        	sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	        jdbcTemplate.update(sql, user.getEmailId(), sb.toString());
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException("Unable to hash password using SHA-512", e);
+		}
 		sql = "SELECT id FROM Login where email = ? AND password = ?";
 		int user_id = jdbcTemplate.queryForObject(sql, Integer.class, user.getEmailId(), user.getPasskey());
 		user.setUser_id(user_id);
@@ -35,8 +47,21 @@ public class SigninDAO implements ISigninDAO{
 	@Override
 	public boolean existsUser(SigninEntity user) {
 		String sql = "SELECT count(*) FROM Login where email = ? AND password = ?";
-		int count = jdbcTemplate.queryForObject(sql, Integer.class, user.getEmailId(), user.getPasskey());
-		if(count == 0)
+		int count = -1;
+		try{
+			MessageDigest m = MessageDigest.getInstance("SHA-512");
+			byte[] bytes = m.digest(user.getPasskey().getBytes());
+			StringBuilder sb = new StringBuilder();
+	        for(int i=0; i< bytes.length ;i++){
+	        	sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	        count = jdbcTemplate.queryForObject(sql, Integer.class, user.getEmailId(), sb.toString());
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException("Unable to hash password using SHA-512", e);
+		}
+		if(count == -1) {
+			throw new IllegalArgumentException("Unable to execute query on the table");
+		} else if(count == 0)
 			return false;
 		else
 			return true;
